33ca0ed4d1f9818794cf20e2aefb7cc1403f0578
Jeremy Endratno <47098124+jeremyendratno@users.noreply.github.com>
2019-12-15 20:58:21 +0700

    tampilan

---
+++ app/src/main/java/com/example/cgram/SplashScreen.java
@@ -0,0 +1,26 @@
+package com.example.cgram;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent MainIntent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(MainIntent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+
+    }
+}
