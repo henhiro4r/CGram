1044d4850da4c22bd537773ae1dcbcf74f5f370c
henhiro4r <32681485+henhiro4r@users.noreply.github.com>
2019-12-15 22:22:33 +0700

    Emoji Done

---
+++ app/src/main/java/com/example/cgram/fragment/EmojiFragment.java
@@ -0,0 +1,65 @@
+package com.example.cgram.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.cgram.R;
+import com.example.cgram.adapter.EmojiAdapter;
+import com.example.cgram.utils.EmojiFragmentListener;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import ja.burhanrashid52.photoeditor.PhotoEditor;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EmojiFragment extends BottomSheetDialogFragment implements EmojiAdapter.EmojiAdapterListener {
+
+    private RecyclerView rvEmoji;
+    static EmojiFragment instance;
+    EmojiFragmentListener listener;
+
+    public void setListener(EmojiFragmentListener listener) {
+        this.listener = listener;
+    }
+
+    public static EmojiFragment getInstance() {
+        if (instance == null) {
+            instance = new EmojiFragment();
+        }
+        return instance;
+    }
+
+    public EmojiFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View itemView = inflater.inflate(R.layout.fragment_emoji, container, false);
+        rvEmoji = itemView.findViewById(R.id.rv_emoji);
+        rvEmoji.setHasFixedSize(true);
+        rvEmoji.setLayoutManager(new GridLayoutManager(getActivity(), 5));
+        EmojiAdapter adapter = new EmojiAdapter(getContext(), PhotoEditor.getEmojis(getContext()), this);
+        rvEmoji.setAdapter(adapter);
+        return itemView;
+    }
+
+    @Override
+    public void onEmojiItemSelecter(String emoji) {
+        listener.onEmojiSelected(emoji);
+    }
+}
